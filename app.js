edc2be5278c730ffd6eb8db29749f1d8dabc954a
umeshkumar223722 <UMESH_223722@SAITM.org>
2024-01-19 17:52:35 +0530

    updated

--- app.js
+++ app.js
@@ -1,12 +1,57 @@
-const temperature = document.querySelector(".temprature input");
-const result = document.querySelector(".result input");
-const inputTempratureUnit = document.querySelector(".temprature select");
-const resultTempratureUnit = document.querySelector(".result select");
 
-temperature.oninput = function(){
-    let inputTempratureUnitValue = inputTempratureUnit.value;
-    let resultTempratureUnitValue = resultTempratureUnit.value;
+function convert(){
+  var fromUnit = document.getElementById('fromUnit').value;
+  var toUnit = document.getElementById('toUnit').value;
+  var temperature = parseFloat(document.getElementById('temperature').value);
 
-    //from Celicius To Other Units
-    let CelciusToKelvin = inputTempratureUnitValue === "celcius" && resultTempratureUnitValue=== "Kelvin";
+  var convertedValue;
+  var resultUnit;
+
+  // Perform conversion logic
+
+  switch (fromUnit){
+      case 'C':
+          if (toUnit === 'F'){
+              convertedValue = (temperature * 9/5) +32;
+              resultUnit = 'F';
+          } else if (toUnit === 'K'){
+              convertedValue = temperature + 273.15;
+              resultUnit = 'K';
+          }else{
+              convertedValue = temperature;
+              resultUnit = 'C';
+          }
+          break;
+
+          case 'F':
+          if (toUnit === 'C'){
+              convertedValue = (temperature -32) * 5/9;
+              resultUnit = 'C';
+          } else if (toUnit === 'K'){
+              convertedValue = (temperature -32) * 5/9 + 273.15;
+              resultUnit = 'K';
+          }else{
+              convertedValue = temperature;
+              resultUnit = 'F';
+          }
+          break;
+
+          case 'K':
+          if (toUnit === 'C'){
+              convertedValue = temperature - 273.15;
+              resultUnit = 'C';
+          } else if (toUnit === 'F'){
+              convertedValue = (temperature - 273.15) * 9/5 +32;
+              resultUnit = 'F';
+          }else{
+              convertedValue = temperature;
+              resultUnit = 'K';
+          }
+          break;
+
+          
+  }
+
+  document.getElementById('result').value = convertedValue.toFixed(2) + ' ' + resultUnit;
+  
 }
